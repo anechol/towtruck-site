e718205071852a17cd9e085653c28b370d7e07d5
anechol <ashleyne92@gmail.com>
2017-03-01 16:20:55 -0600

    Adds testimonials to About page.

--- app/js/components/About.js
+++ app/js/components/About.js
@@ -10,9 +10,19 @@ export default React.createClass({
 					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 				</div>
 				<div className='container'>
-					<h2>Testimonials</h2>
-					<img className='rounded float-left'/>
-					<blockquote>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</blockquote>
+					<h2>See What Our Customers Think</h2>
+					<div className='test-left d-flex align-items-center'>
+						<img src='http://lorempixel.com/150/150/animals/6' className='img-thumbnail float-left'/>
+						<blockquote>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</blockquote>
+					</div>
+					<div className='test-right d-flex align-items-center'>
+						<img src='http://lorempixel.com/150/150/animals/1' className='img-thumbnail'/>
+						<blockquote>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</blockquote>
+					</div>
+					<div className='test-left d-flex align-items-center'>
+						<img src='http://lorempixel.com/150/150/animals/8' className='img-thumbnail'/>
+						<blockquote>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</blockquote>
+					</div>
 				</div>
 			</section>
 		)
